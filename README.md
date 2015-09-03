8f1cc3db2456dcc00fcc4a3d6fac438247eb43c8
O'meid <public@omeid.me>
2015-09-03 16:32:20 +1000

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+#Resources [![GoDoc](https://img.shields.io/badge/godoc-reference-blue.svg?style=flat-square)](https://godoc.org/github.com/go-gonzo/resources)
+Package `resources` provides bindings to [go-resources](https://github.com/omeid/go-resources) which allows embeding of static resources in Go programs.
---
+++ resources.go
@@ -0,0 +1,56 @@
+package resources
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"path/filepath"
+	"strings"
+
+	"github.com/omeid/gonzo/context"
+
+	"github.com/omeid/go-resources"
+	"github.com/omeid/gonzo"
+)
+
+type Config resources.Config
+
+var FilenameFormat = "%s_resource.go"
+
+// A build stage creates a new Package and adds all the files coming through the channel to
+// the package and returns the result of build as a File on the output channel.
+func Build(config Config) gonzo.Stage {
+	return func(ctx context.Context, in <-chan gonzo.File, out chan<- gonzo.File) error {
+
+		res := resources.New()
+		res.Config = resources.Config(config)
+
+		for {
+			select {
+			case file, ok := <-in:
+				if !ok {
+					break
+				}
+				path, _ := filepath.Rel(file.FileInfo().Base(), file.FileInfo().Name())
+				res.Add(filepath.ToSlash(path), file)
+				ctx.Infof("Adding %s.\n", path)
+				defer file.Close() //Close files AFTER we have build our package.
+			case <-ctx.Done():
+				return ctx.Err()
+			}
+		}
+
+		var buff *bytes.Buffer
+		err := res.Build(buff)
+		if err != nil {
+			return err
+		}
+
+		path := fmt.Sprintf(FilenameFormat, strings.ToLower(res.Var))
+		sf := gonzo.NewFile(ioutil.NopCloser(buff), gonzo.NewFileInfo())
+		sf.FileInfo().SetName(path)
+		sf.FileInfo().SetSize(int64(buff.Len()))
+		out <- sf
+		return nil
+	}
+}
