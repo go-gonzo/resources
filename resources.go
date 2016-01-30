9e29fd26d198b9be04d25f5aa2182510d8c23d28
O'meid <public@omeid.me>
2016-01-30 13:55:32 +1100

    Better build and logging.

--- resources.go
+++ resources.go
@@ -20,41 +20,55 @@ var FilenameFormat = "%s_resource.go"
 // A build stage creates a new Package and adds all the files coming through the channel to
 // the package and returns the result of build as a File on the output channel.
 func Build(config Config) gonzo.Stage {
-	return func(ctx context.Context, in <-chan gonzo.File, out chan<- gonzo.File) error {
+	return func(ctx context.Context, files <-chan gonzo.File, out chan<- gonzo.File) error {
 
-		res, err := build(ctx, in, config)
+		ctx, cancel := context.WithCancel(ctx)
 
-		var buff *bytes.Buffer
-		err = res.Build(buff)
+		res := resources.New()
+		res.Config = resources.Config(config)
+
+		var err error 
+
+		buff := &bytes.Buffer{}
+		for {
+			select {
+			case file, ok := <-files:
+				if !ok {
+					goto BUILD
+				}
+
+				if file.FileInfo().IsDir() {
+					continue
+				}
+				path, _ := filepath.Rel(file.FileInfo().Base(), file.FileInfo().Name())
+				res.Add(filepath.ToSlash(path), file)
+				ctx.Infof("Adding %s", path)
+				defer func(path string) {
+					ctx.Debug("Closing %s", path)
+					file.Close() //Close files AFTER we have build our package.
+				}(path)
+			case <-ctx.Done():
+				err = ctx.Err()
+				goto BUILD
+			}
+		}
+
+		BUILD:
 		if err != nil {
 			return err
 		}
 
-		path := fmt.Sprintf(FilenameFormat, strings.ToLower(res.Var))
+		ctx.Debug("Runnig build...")
+		err = res.Build(buff)
+		if err != nil {
+			cancel()
+			return err
+		}
+		path := fmt.Sprintf(FilenameFormat, strings.ToLower(config.Var))
 		sf := gonzo.NewFile(ioutil.NopCloser(buff), gonzo.NewFileInfo())
 		sf.FileInfo().SetName(path)
 		sf.FileInfo().SetSize(int64(buff.Len()))
 		out <- sf
 		return nil
-	}
-}
-
-func build(ctx context.Context, files <-chan gonzo.File, config Config) (*resources.Package, error) {
-
-	res := resources.New()
-	res.Config = resources.Config(config)
-	for {
-		select {
-		case file, ok := <-files:
-			if !ok {
-				return res, nil
-			}
-			path, _ := filepath.Rel(file.FileInfo().Base(), file.FileInfo().Name())
-			res.Add(filepath.ToSlash(path), file)
-			ctx.Infof("Adding %s.\n", path)
-			defer file.Close() //Close files AFTER we have build our package.
-		case <-ctx.Done():
-			return res, ctx.Err()
-		}
 	}
 }
